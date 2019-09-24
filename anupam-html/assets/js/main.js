e0b0033ddc03c2cca1f8a57986a3a1e577dfc60a
vivek <nbvikky@gmail.com>
2019-09-24 18:35:35 +0530

    updated anu HTML files

---
+++ anupam-html/assets/js/main.js
@@ -0,0 +1,7 @@
+function Hello(){
+	// alert("Hello"+ " " + "\n"+"ashoka");
+	var inputEmailText = document.getElementById('inputText').value;
+	document.getElementById("inputEmail").value = inputEmailText
+	// alert(inputEmailText);
+	console.log(inputEmailText);
+	}
